7f482c11af9529d8564e02fafd6276ec418e319c
huliang <huliang1107@163.com>
2017-12-14 10:53:29 +0800

    first commit

---
+++ js/Main.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Right, Button, Text, View, Touchable, Image, Left, Header, Title, Body, Container, Content } from "./components";
+export default class App extends React.Component {
+	render() {
+		return (
+			<Container>
+				<Header>
+					<Text>首页</Text>
+				</Header>
+				<Content>
+					<View style={styles.container}>
+						<Text>Changes you make will automatically reload.</Text>
+						<Text>Shake your phone to open the developer menu.</Text>
+					</View>
+				</Content>
+			</Container>
+
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});
---
+++ js/components/base/index.js
@@ -0,0 +1,7 @@
+export { StyleSheet, TextInput, Platform, ActivityIndicator, Alert, Keyboard, Modal, Dimensions } from 'react-native';
+export {
+	Header, List, ListItem, SearchBar, Left, Body, Right, Thumbnail, Icon, Container,
+	Content, Tab, TabHeading, Form, Item, Label, Fab, Card, CardItem, Title, StyleProvider,
+	H1, H2, H3, View, Text, CheckBox, Textarea, Badge, Picker, Radio, Root, SwipeRow
+
+} from "native-base";
